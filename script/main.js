b9bcbad5ec56a68caa5e8cd6bf4dad272e1c021a
trekerz <zjf007@qq.com>
2016-09-21 01:09:41 +0800

    success in changing head-card!

--- script/main.js
+++ script/main.js
@@ -1,4 +1,27 @@
-// 点击日期后发生的事件
-function clickItem (){
-	document.getElementById("")
+// hover和click日期后发生的动画
+var currentClicked = $(".hovered");
+var clickedOrNot = false;
+
+function hoverinItem (){
+	currentClicked.removeClass("hovered");
+	$(this).addClass("hovered");
 }
+
+function hoveroutItem (){
+	if(clickedOrNot == false){
+		$(this).removeClass("hovered");
+		currentClicked.addClass("hovered");
+	}else{
+		clickedOrNot = false;
+	}
+}
+
+function clickItem (){
+	currentClicked.removeClass("hovered");
+	$(this).addClass("hovered");
+	clickedOrNot = true;
+	currentClicked = $(this);
+}
+
+$(".head-card-item").hover(hoverinItem,hoveroutItem);
+$(".head-card-item").click(clickItem);
